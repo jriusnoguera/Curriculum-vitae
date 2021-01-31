55f5d9369a329d9193e518eeae40725a573de975
jriusnoguera <77150554+jriusnoguera@users.noreply.github.com>
2021-01-31 20:58:46 +0100

    commit

--- Curriculum/main.js
+++ Curriculum/main.js
@@ -21,11 +21,11 @@ function cambiaColoronBlur() {
 function functOnInput() {
     var telContact = document.getElementById("telContact").value;
     if (telContact.length > 9) {
-    document.getElementById("infotel").innerHTML = `El número de dígitos es: ${telContact.length} , el número demasiado largo` ;
+    document.getElementById("infotel").innerHTML = `El número de dígitos es: ${telContact.length} , demasiado largo` ;
     } else if (telContact.length < 9 ) {
-    document.getElementById("infotel").innerHTML = `El número de dígitos es: ${telContact.length} , el número demasiado corto` ;
+    document.getElementById("infotel").innerHTML = `El número de dígitos es: ${telContact.length} , demasiado corto` ;
     } else {
-    document.getElementById("infotel").innerHTML = `El número de dígitos es: ${telContact.length} , el número es correcto`
+    document.getElementById("infotel").innerHTML = `El número de dígitos es: ${telContact.length} ,  es correcto`
     }
 };
 
@@ -36,9 +36,40 @@ function functemailInput (){
     if (mailContact !=mailComp) {
         document.getElementById("infomail").innerHTML = `El mail no es coincidente` ;
     } else {
-        document.getElementById("infomail").innerHTML = `El mail es correcto`
+        document.getElementById("infomail").innerHTML = `El mail es correcto`;
     }
+};
+
+function mouseover() {
+    document.getElementById("formacion").innerHTML = `Detalle de mi formación`
+   };
+function mouseout() {
+    document.getElementById("formacion").innerHTML = `Formación`
+};   
+function mouseover1() {
+     document.getElementById("contacto").innerHTML = `Contacta conmigo`
+};
+function mouseout1() {
+    document.getElementById("contacto").innerHTML = `Contacto`;
+}
+function mouseover2() {
+    document.getElementById("proyectos").innerHTML = `Mis proyectos`
+};
+function mouseout2() {
+   document.getElementById("proyectos").innerHTML = `Proyectos`;
+}
+function mouseover3() {
+    document.getElementById("experiencia").innerHTML = `Mi trabajo`
+};
+function mouseout3() {
+   document.getElementById("experiencia").innerHTML = `Experiencia laboral`;
 }
 
+
+
+
+
+
+  
   
  
